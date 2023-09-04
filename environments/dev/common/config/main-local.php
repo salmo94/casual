794fc94138cc4510cdaa845e42c3fefa9ecd3a3c
salmo94 <salmo.94@ukr.net>
2023-09-04 20:37:41 +0300

    adding attributes for categories(work with JS).

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -2,13 +2,6 @@
 
 return [
     'components' => [
-        'db' => [
-            'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@common/mail',
