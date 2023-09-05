4ede64050c571b34b278b07f7a391f7eb653cf1a
salmo94 <salmo.94@ukr.net>
2023-09-05 13:29:29 +0300

    change 2

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -2,13 +2,6 @@
 
 return [
     'components' => [
-        'db' => [
-            'class' => \yii\db\Connection::class,
-            'dsn' => getenv('CASUAL_POSTGRES_DSN'),
-            'username' => getenv('CASUAL_POSTGRES_USER'),
-            'password' => getenv('CASUAL_POSTGRES_PASSWORD'),
-            'charset' => 'utf8',
-        ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@common/mail',
